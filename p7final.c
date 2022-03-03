797f75b18ab737cfe2759e08c4d7aa72e78bd930
BM Prajwal <89903153+bmprajwal@users.noreply.github.com>
2022-03-03 08:50:50 +0000

    .

--- p7final.c
+++ p7final.c
@@ -6,6 +6,9 @@ int num, den;
 typedef struct fraction Fraction;
 
 int gcd(int a, int b);
+Fraction input_fraction();
+Fraction add_fractions(Fraction f1, Fraction f2);
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction sum);
 
 int main(){
   printf("%d\n", gcd(16,24));

67cfb3cf346a0d73f9a22b4b9f4380e82a231216
BM Prajwal <89903153+bmprajwal@users.noreply.github.com>
2022-03-03 08:49:24 +0000

    .

--- p7final.c
+++ p7final.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+struct fraction {
+int num, den;
+};
+typedef struct fraction Fraction;
+
+int gcd(int a, int b);
+
+int main(){
+  printf("%d\n", gcd(16,24));
+}
+int gcd(int a, int b){
+  int t;
+  while(b!=0){
+    t = b;
+    b = a%b;
+    a = t;
+  }
+  return a;
+}
